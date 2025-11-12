b3d97902df2568dff3db10657bc037b303d581ec
453717310 <453717310@qq.com>
2025-11-12 08:38:22 +0800

    send  ETH

--- base/EtherSendDemo.sol
+++ base/EtherSendDemo.sol
@@ -4,7 +4,34 @@ pragma solidity ^0.8.20;
 contract EtherSendDemo{
 
     // 来调用目标合约：transfer、send 或 call。
-
     // transfer 发送失败会抛异常
     // send call 发送失败会返回 bool false
+
+    int256 public count;
+
+    function decrement(int num) public payable {
+        count = count -num;
+    }
+
+    fallback() external payable {
+        count ++;
+    }
+
+    receive() external payable {
+        count +=5;
+    }
+
+}
+
+interface IEtherSendDemo {
+    function decrement(int num) external payable ;
+    function nonExistentFunction() external ;
+}
+
+contract EtherSendDemoCaller{
+
+    function callFallback(address _target) public {
+        IEtherSendDemo demo = IEtherSendDemo(_target);
+        demo.nonExistentFunction();
+    }
 }
